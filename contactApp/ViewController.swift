7280b53bbfed425a2e9c67c3d7aa717d213beee2
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-27 23:33:34 +0300

    Initial Commit

---
+++ contactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactApp
+//
+//  Created by slava on 27.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
