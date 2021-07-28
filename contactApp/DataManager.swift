498fd089fe9e01e8e42bd5b7f18317a5018ccc74
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-28 23:56:05 +0300

    Fixed random generation

--- contactApp/DataManager.swift
+++ contactApp/DataManager.swift
@@ -1,48 +1,44 @@
 
-
 class DataManager {
-    var persons: [Person]?
+    var persons: [Person] = []
     
-    private static let dataManager = DataManager()
+    static let shared = DataManager()
     
-    private var names: [String] = ["John", "Sven", "Kate", "Carl", "Alice", "Mary", "Abraham", "Elliah"]
-    private var surnames: [String] = ["Anderson", "Smith", "Doe", "Katz", "Jones", "Ramirez", ""]
-    private var numbers: [String] = []
-    private var emails: [String] = []
+    private var names: [String] = ["John", "Sven", "Kate", "Carl", "Alice", "Mary", "Abraham", "Elliah", "Ariel", "Nataly", "Amanda", "Randy", "Barbara", "Henry", "Michael"]
+    private var surnames: [String] = ["Anderson", "Smith", "Doe", "Katz", "Jones", "Ramirez", "Ramone", "Armstrong", "James", "Adams", "Johnson", "Daniels", "Samuels", "Rogers", "Jackson"]
     
-    public static func shared() -> DataManager {
-        dataManager
-    }
-    
-    private func generateData(count: Int) {
+    func generateData(count: Int) {
         var randomNames: [String: Bool] = [:]
         var randomSurnames: [String: Bool] = [:]
         var randomNumbers: [String: Bool] = [:]
         var randomEmails: [String: Bool] = [:]
         
-        for var iteration in 0...count {
-            guard let name = names.randomElement() else { return }
-            guard let surname = surnames.randomElement() else { return }
-            let number = generatePhoneNumber()
-            let email = generateEmail(name: name, surname: surname)
-            
-            if !randomNames.keys.contains(name) && !randomSurnames.keys.contains(surname) &&
-                !randomNumbers.keys.contains(number) && !randomEmails.keys.contains(email) {
+        for iteration in 0...count {
+            print("generation iterations is: \(iteration)")
+            iterationReset: do {
+                guard let name = names.randomElement() else { return }
+                guard let surname = surnames.randomElement() else { return }
+                let number = generatePhoneNumber()
+                let email = generateEmail(name: name, surname: surname)
                 
-                randomNames[name] = true
-                randomSurnames[surname] = true
-                randomNumbers[number] = true
-                randomEmails[email] = true
-                
-                persons?.append(Person(name: name, surname: surname, number: number, email: email))
-                continue
+                if !randomNames.keys.contains(name) && !randomSurnames.keys.contains(surname) &&
+                    !randomNumbers.keys.contains(number) && !randomEmails.keys.contains(email) {
+                    
+                    randomNames[name] = true
+                    randomSurnames[surname] = true
+                    randomNumbers[number] = true
+                    randomEmails[email] = true
+                    
+                    persons.append(Person(name: name, surname: surname, number: number, email: email))
+                    continue
+                }
+                continue iterationReset
             }
-            iteration -= 1
         }
         
         func generatePhoneNumber() -> String {
             let randomNumber = Int.random(in: 1111111111...9999999999)
-            return "+7 " + String(randomNumber)
+            return "+7" + String(randomNumber)
         }
         
         func generateEmail(name: String, surname: String) -> String {
---
+++ contactApp/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  contactApp
+//
+//  Created by slava on 28.07.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
